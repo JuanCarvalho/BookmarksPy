8f0e4a4b54130d17437869751ab84d544bbec8dc
juancarvalho <juan.carvalho82@gmail.com>
2017-02-26 23:52:07 -0300

    Commit inicial, mostrando exemplos no README, adicionando comentarios no random_bookmarks_firefox.py

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # BookmarksPy
-Open Randomize Bookmarks using python
+
+Busca uma url randomicamente nos favoritos do firefox, e abre no navegador padrao. 
+Search for a random url in firefox favorites, and open in the default browser.
+
+
+Example 1, list all folders containing links:
+
+    $ python random_bookmarks_firefox.py -l
+    ID
+    2   Menu de favoritos
+    3   Barra de favoritos
+    5   Outros favoritos
+    7   Ubuntu and Free Software links
+    12   Mozilla Firefox
+
+
+Example 2, show links and open a url of ID 7 (7  Ubuntu and Free Software links): 
+
+    $ python random_bookmarks_firefox.py -i 7
+
+    Pasta pesquisada : Ubuntu and Free Software links
+
+    http://www.ubuntu.com/
+    http://wiki.ubuntu.com/
+    https://answers.launchpad.net/ubuntu/+addquestion
+    http://www.debian.org/
+
+
+Example 3, Searches for folders containing:
+
+    $ python random_bookmarks_firefox.py -p Ubuntu
+
+    Busca as pastas que contenham :  Ubuntu
+
+    Pasta(s) pesquisada(s) :
+    Ubuntu and Free Software links
+
+    http://www.ubuntu.com/
+    http://wiki.ubuntu.com/
+    https://answers.launchpad.net/ubuntu/+addquestion
+    http://www.debian.org/
+
+
+
+Esse é meu primeiro script no github, estou aprendendo a usar essa ferramenta.
+This is my first github script, I'm learning to use this tool.
+
+contato: juan.carvalho82@gmail.com
--- random_bookmarks_firefox.py
+++ random_bookmarks_firefox.py
@@ -6,11 +6,12 @@ __license__ = "GPL"
 __version__ = "1.0.1"
 __maintainer__ = "Carlos A. B. Carvalho"
 __email__ = "juan.carvalho82@gmail.com"
-__status__ = "Production"
+__status__ = "Test"
 __python__ = "2.7"
 
 """
-    Busca uma url randomicamente nos favoritos do firefox, e abre no navegador padrao 
+    Busca uma url randomicamente nos favoritos do firefox, e abre no navegador padrao. 
+    Search for a random url in firefox favorites, and open in the default browser.
 """
 
 import random
@@ -23,22 +24,6 @@ import traceback
 # Caminho para o arquivo places.sqlite
 db = sqlite3.connect("/home/juan/.mozilla/firefox/odwanb5d.default/places.sqlite")
 
-# O banco moz_places armazenam historico, favoritos e outras coisas...
-# O nome do banco dos sites favoritos é moz_bookmarks
-# As chaves estrangeiras (fk) do banco moz_bookmarks, são os id's do banco moz_places
-
-# EXEMPLO DE COMO SÃO SALVOS OS DADOS NOS FAVORITOS (moz_bookmarks)
-# id == Chave unica
-# type == tipo de arquivo 1(link), 2(folder)
-# fk == id no banco moz_places 
-# parent == id no banco moz_bookmarks
-
-# id|type|fk|parent|position|title|keyword_id|folder_type|dateAdded|lastModified|guid
-# 24|2||2|4|Django|||1466105656533000|1466105658578000|CGrTi1bmEIsa
-### o parent do favorito abaixo é o id da pasta onde ele esta localizado (parent 24 pertence à pasta com o id 24, "Django")	
-# 35|1|20|24|3|Django: Image Files Uploading Example - 2015|||1466105656980000|1466105656980000|Er_Xu1_LagIa
-
-
 parser = argparse.ArgumentParser()
 
 parser.add_argument("-l", "--listar", help="Lista as pastas dos favoritos", action="store_true")
@@ -46,7 +31,6 @@ parser.add_argument("-p", "--pasta", help="Busca por pastas que contenham o term
 parser.add_argument("-s", "--search", help="Busca links que contenham o termo", action="store_true")
 parser.add_argument("-i","--id", help="Busca pelo id da pasta", action="store_true")
 parser.add_argument("-a","--all", help="Busca uma pagina aleatória em todas as pastas", action="store_true")
-# opcao nargs=? significa argumento opcional
 parser.add_argument("Arg",help="Termo para busca", type=str, nargs='?',)
 
 # Se não houver arg mostra o help
@@ -103,16 +87,23 @@ if args.pasta:
 # Lista de pastas que não retornam srings
 lista_ignorados = ["", "Tags", "Favoritos", "mobile"]
 if args.listar:
-	id_tags = ''
-	pastas = db.execute("select * from moz_bookmarks where type=2").fetchall()
-	print "ID"
-	for i in pastas:
-		if i[5].encode('utf-8').lower() == 'tags':
-			id_tags = i[0]
-		if i[3] != id_tags:
-			# Não printar itens da lista de pastas que não retornam url's
-			if not i[5].encode('utf-8') in lista_ignorados:
-				print i[0], " ", i[5].encode('utf-8')
+    id_tags = ''
+    ultimo_id_mostrado = ''
+    pastas = db.execute("select * from moz_bookmarks where type=2").fetchall()
+    print 
+    print "ID"
+    for i in pastas:
+        if i[5].encode('utf-8').lower() == 'tags':
+            id_tags = i[0]
+        if i[3] != id_tags:
+            # Não printar itens da lista de pastas que não retornam url's
+            if not i[5].encode('utf-8') in lista_ignorados:
+                # Save the last id to show in the example below
+                ultimo_id_mostrado = i[0]
+                print i[0], " ", i[5].encode('utf-8')
+    print
+    print "Try using: $ python random_bookmarks_firefox.py -i {0}".format(ultimo_id_mostrado)
+    print
 
 if args.id:
 	lista_de_pastas = {}
@@ -172,10 +163,14 @@ if args.all:
 		except:
 			traceback.format_exc()
 
-
-
 	new = 2 # open in a new tab, if possible
 	# Busca uma url na lista_url, um indice randomico que seja 1 valor a menos que o tamanho da lista
 	url = lista_url[random.randrange(len(lista_url) - 1)]
 	# Abre o navegador padrão
 	webbrowser.open(url,new=new)
+
+if args.search:
+    # Buscar links que contenham a palavra recebida como argumento
+    # Find links containing the word received as an argument
+    print "Under development"
+    print "Find links containing the word received as an argument"
